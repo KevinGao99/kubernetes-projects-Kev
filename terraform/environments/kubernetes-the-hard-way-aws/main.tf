72a85014a1937c13764ff0aeb4f3483b34ad3201
SalmonEater <120135123+KevinGao99@users.noreply.github.com>
2026-06-21 18:04:46 +0000

    Change region

--- terraform/environments/kubernetes-the-hard-way-aws/main.tf
+++ terraform/environments/kubernetes-the-hard-way-aws/main.tf
@@ -8,13 +8,13 @@ terraform {
   backend "s3" {
     bucket       = "terraform-state-${local.aws_region}-${local.aws_account_id}"
     key          = "terraform.tfstate"
-    region       = local.aws_region
+    region       = "us-east-2"
     use_lockfile = true
   }
 }
 
 # Configure the AWS Provider
 provider "aws" {
-  region = local.aws_region
+  region = "us-east-2"
 }
 
